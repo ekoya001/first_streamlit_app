128b201b67d9f716f278b4e25d6960bc7104339b
koya <108051565+ekoya001@users.noreply.github.com>
2022-06-23 18:21:55 +0900

    Update streamlit_app.py
    added Emojis to headder

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Halthy Diner')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
